3a987357518a3f5618928ddf1d1106061dca310c
juhis <juhis@NiJu>
2024-08-16 11:55:26 +0530

    Bulid updated

---
+++ jenkins_session/src/test/java/jenkins_package/PollScMTest.java
@@ -0,0 +1,13 @@
+package jenkins_package;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PollScMTest {
+	@Test
+	public void poll()
+	{
+		Reporter.log("poll scm cls added and executed",true);
+	}
+
+}
